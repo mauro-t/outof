e7d1a8b6b5df11461e885b4ce9a5dd7c5b5c821a
mauroooo <orzofont@gmail.com>
2025-10-04 15:52:31 +0200

    better split text

--- src/animations/animate-text.ts
+++ src/animations/animate-text.ts
@@ -1,12 +1,25 @@
 import { animate, inView, stagger } from "motion";
 import SplitType from "split-type";
+import { debounce } from "lodash";
 
 export default function animateText() {
   const elements = document.querySelectorAll<HTMLElement>("[data-text]");
   elements.forEach((element) => {
     const textType = element.dataset.text;
     const originalHTML = element.innerHTML;
-    const { lines } = new SplitType(element, { types: "lines" });
+    const text = new SplitType(element, { types: "lines" });
+    let animated = false;
+    let w: number | undefined = undefined;
+    const resizeObserver = new ResizeObserver(
+      debounce(([entry]) => {
+        w ??= entry.contentRect.width;
+        if (w != entry.contentRect.width && !animated) {
+          text.split({ types: "lines" });
+          w = entry.contentRect.width;
+        }
+      }, 500),
+    );
+    resizeObserver.observe(element);
 
     inView(
       element,
@@ -16,19 +29,19 @@ export default function animateText() {
             ? { y: ["100%", 0] }
             : { y: ["0.5em", 0], opacity: [0, 1] };
         const duration = textType == "title" ? 0.6 : 0.75;
-        if (lines) {
-          animate(lines, animationKeyframes, {
-            duration,
-            delay: stagger(0.13),
-            ease: "easeOut",
-          }).then(() => {
-            element.classList.remove("overflow-hidden");
-            element.innerHTML = originalHTML;
-          });
-          setTimeout(() => {
-            element.style.opacity = "1";
-          }, 50);
-        }
+
+        animate(element.querySelectorAll(".line"), animationKeyframes, {
+          duration,
+          delay: stagger(0.13),
+          ease: "easeOut",
+        }).then(() => {
+          element.classList.remove("overflow-hidden");
+          element.innerHTML = originalHTML;
+          animated = true;
+        });
+        setTimeout(() => {
+          element.style.opacity = "1";
+        }, 50);
       },
       {
         margin: "0px 0px -200px 0px",
