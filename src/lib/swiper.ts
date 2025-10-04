4dc187d20a076eaca9b97c829a23952b9e9cfa36
mauroooo <orzofont@gmail.com>
2025-10-04 14:46:49 +0200

    sliders

--- src/lib/swiper.ts
+++ src/lib/swiper.ts
@@ -1,11 +1,44 @@
 import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
 
-export function initSwiper() {
+export function initSwipers() {
   new Swiper(".swiper", {
     spaceBetween: 12,
     breakpoints: {
       768: {
         slidesPerView: "auto",
+        slidesOffsetBefore: 20,
+        slidesOffsetAfter: 20,
+      },
+    },
+    slidesPerView: 1.15,
+    slidesOffsetBefore: 12,
+    slidesOffsetAfter: 12,
+  });
+
+  new Swiper(".swiper-fullscreen", {
+    modules: [Pagination],
+    breakpoints: {
+      768: {
+        slidesOffsetBefore: 20,
+        slidesOffsetAfter: 20,
+      },
+    },
+    slidesOffsetBefore: 12,
+    slidesOffsetAfter: 12,
+    pagination: {
+      el: ".swiper-pagination",
+      bulletClass: "size-2 bg-white rounded-full inline-block",
+      bulletActiveClass: "size-4 !bg-accent",
+      clickable: true,
+    },
+  });
+
+  new Swiper(".swiper-project-block", {
+    spaceBetween: 12,
+    breakpoints: {
+      768: {
+        slidesPerView: 2.5,
         slidesOffsetBefore: 20,
         slidesOffsetAfter: 20,
       },
