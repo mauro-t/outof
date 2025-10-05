956c8a9773158c9807faf5c960add4cd79275099
mauroooo <orzofont@gmail.com>
2025-10-05 15:41:43 +0200

    asdasf

--- src/scripts/popup-newsletter.ts
+++ src/scripts/popup-newsletter.ts
@@ -2,18 +2,20 @@ import { animate } from "motion";
 
 export default function PopupNewsletter() {
   const newsletter = document.getElementById("newsletter-popup") as HTMLElement;
-  const trigger = document.querySelector<HTMLElement>(
+  const triggers = document.querySelectorAll<HTMLElement>(
     "[data-newsletter-trigger]",
   );
   const close = document.querySelector<HTMLElement>("[data-newsletter-close]");
 
-  trigger?.addEventListener("click", () => {
-    animate(
-      newsletter,
-      { opacity: 1, pointerEvents: "all" },
-      { duration: 0.45 },
-    );
-  });
+  triggers.forEach((trigger) =>
+    trigger.addEventListener("click", () => {
+      animate(
+        newsletter,
+        { opacity: 1, pointerEvents: "all" },
+        { duration: 0.45 },
+      );
+    }),
+  );
 
   close?.addEventListener("click", () => {
     animate(
