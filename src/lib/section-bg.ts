42c2480d7ef2fd1fc190ec33166650d71bf9b664
mauroooo <orzofont@gmail.com>
2025-10-09 15:19:17 +0200

    okok

--- src/lib/section-bg.ts
+++ src/lib/section-bg.ts
@@ -1,8 +1,11 @@
-export function sectionBg(bg: "dark" | "light" | "mask" | "ellipses") {
+export function sectionBg(
+  bg: "dark" | "light" | "mask" | "ellipses" | "mask-not-inverted",
+) {
   const classes = {
     dark: "bg-black text-white dark:bg-accent dark:text-black",
     light: "bg-white dark:bg-black",
     mask: "bg-white dark:bg-black",
+    "mask-not-inverted": "bg-black text-white dark:bg-accent dark:text-black",
     ellipses: "bg-black text-white dark:bg-accent dark:text-black ",
   } as const;
 
