50c6d671a017a82ac949fa9aa7cbd00f1c440306
mauroooo <orzofont@gmail.com>
2025-10-11 15:02:01 +0200

    ok ekei

--- src/scripts/homepage-hero.ts
+++ src/scripts/homepage-hero.ts
@@ -1,27 +1,83 @@
-import { animate } from "motion";
+import { animate, stagger } from "motion";
+import SplitType from "split-type";
 
 export default function HomepageHero() {
-  const titleWords = document.getElementById("title-words") as HTMLElement;
+  const titleWords =
+    document.querySelectorAll<HTMLElement>("#title-words > div");
   const titleParagraphs = document.querySelectorAll<HTMLElement>(
     "#title-paragraphs p",
   );
 
-  let i = 1;
+  let i = 0;
 
-  setInterval(() => {
-    animate([
-      [titleWords, { y: `-${i * 100}%` }, { duration: 0.4 }],
-      [titleParagraphs[i - 1], { opacity: 0 }, { at: "<" }],
-      [titleParagraphs[i], { opacity: 1 }, { at: "<" }],
-    ]).then(() => {
-      if (i == 6) {
-        animate(titleWords, { y: 0 }, { duration: 0 });
-        animate(titleParagraphs[0], { opacity: 1 }, { duration: 0 });
-        animate(titleParagraphs[i], { opacity: 0 }, { duration: 0 });
-        i = 1;
-      } else {
-        i++;
-      }
+  titleWords.forEach((word, i) => {
+    const { chars } = new SplitType(word, { types: "chars" });
+    chars?.forEach((char) => {
+      char.style.opacity = "0";
+      char.style.display = "inline-flex";
     });
-  }, 3000);
+  });
+
+  titleParagraphs.forEach((paragraph, i) => {
+    const { lines } = new SplitType(paragraph, { types: "lines" });
+    lines?.forEach((line) => {
+      line.style.opacity = "0";
+    });
+  });
+
+  async function showWordChars() {
+    const wordChars = [...titleWords[i].querySelectorAll(".char")];
+    animate(
+      wordChars,
+      { opacity: 1 },
+      { delay: stagger(0.075), duration: 0.05 },
+    ).then(() => {
+      animate(
+        wordChars,
+        { opacity: 0 },
+        { delay: stagger(0.075, { startDelay: 1.2 }), duration: 0.05 },
+      );
+    });
+  }
+
+  async function showParagraphWords() {
+    const paragraphWords = [...titleParagraphs[i].querySelectorAll(".line")];
+    animate(
+      paragraphWords,
+      { opacity: 1, y: [10, 0] },
+      { delay: stagger(0.175), duration: 0.2, ease: "easeInOut" },
+    ).then(() => {
+      animate(
+        paragraphWords,
+        { opacity: 0, y: [0, -10] },
+        {
+          duration: 0.2,
+          ease: "easeInOut",
+          delay: stagger(0.05, { startDelay: 1.2 }),
+        },
+      );
+    });
+  }
+
+  let time;
+
+  function loop(timestamp: number) {
+    time ??= timestamp;
+    if (timestamp - time >= 2000) {
+      showWordChars();
+      showParagraphWords();
+      if (i == titleWords.length - 1) i = 0;
+      else i += 1;
+      time = timestamp;
+    }
+    requestAnimationFrame(loop);
+  }
+  setTimeout(() => {
+    requestAnimationFrame(loop);
+
+    showWordChars();
+    showParagraphWords();
+    if (i == titleWords.length - 1) i = 0;
+    else i += 1;
+  }, 400);
 }
