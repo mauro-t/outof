39095c87c5b57ba42b19130c531a6c03355fca9a
mauroooo <orzofont@gmail.com>
2025-10-21 14:50:45 +0200

    fixed

--- src/scripts/popup-newsletter.ts
+++ src/scripts/popup-newsletter.ts
@@ -8,16 +8,36 @@ export default function PopupNewsletter() {
   const close = document.querySelector<HTMLElement>("[data-newsletter-close]");
 
   triggers.forEach((trigger) =>
-    trigger.addEventListener("click", () => {
-      animate(
+    trigger.addEventListener("click", async () => {
+      //@ts-ignore
+      window.lenis.stop();
+
+      await animate(
         newsletter,
         { opacity: 1, pointerEvents: "all" },
         { duration: 0.45 },
-      );
+      ).finished;
     }),
   );
 
+  newsletter.addEventListener("click", (e) => {
+    if (
+      e.target instanceof Node &&
+      newsletter.firstElementChild?.contains(e.target)
+    )
+      return;
+    //@ts-ignore
+    window.lenis.start();
+    animate(
+      newsletter,
+      { opacity: 0, pointerEvents: "none" },
+      { duration: 0.45 },
+    );
+  });
+
   close?.addEventListener("click", () => {
+    //@ts-ignore
+    window.lenis.start();
     animate(
       newsletter,
       { opacity: 0, pointerEvents: "none" },
