8c48f22d3758834c9a4ed67a1e8addc0c1020f81
mauroooo <orzofont@gmail.com>
2025-10-22 16:12:55 +0200

    asd

--- src/scripts/popup-newsletter.ts
+++ src/scripts/popup-newsletter.ts
@@ -6,17 +6,28 @@ export default function PopupNewsletter() {
     "[data-newsletter-trigger]",
   );
   const close = document.querySelector<HTMLElement>("[data-newsletter-close]");
+  const video = newsletter.querySelector<HTMLVideoElement>("video");
 
   triggers.forEach((trigger) =>
     trigger.addEventListener("click", async () => {
       //@ts-ignore
       window.lenis.stop();
 
-      await animate(
+      animate(
         newsletter,
         { opacity: 1, pointerEvents: "all" },
         { duration: 0.45 },
-      ).finished;
+      );
+      // animate(0, video!.duration, {
+      //   duration: video?.duration,
+      //   repeat: Infinity,
+      //   repeatType: "reverse",
+      //   onUpdate: (latest) => {
+      //     if (!video) return;
+      //     video.currentTime = latest;
+      //     console.log(video.currentTime);
+      //   },
+      // });
     }),
   );
 
